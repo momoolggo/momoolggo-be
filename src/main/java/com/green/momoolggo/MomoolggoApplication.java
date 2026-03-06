061035f2a134bc5ff7be8a3fc437b4282445067e
권준하 <kjh980816@gmail.com>
2026-03-06 10:50:36 +0900

    first

---
+++ src/main/java/com/green/momoolggo/MomoolggoApplication.java
@@ -0,0 +1,13 @@
+package com.green.momoolggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoolggoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomoolggoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/momoolggo/MomoolggoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.momoolggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoolggoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
