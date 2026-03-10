57c276d9078c710ef3ab429ac2575941b4b9572d
권준하 <kjh980816@gmail.com>
2026-03-10 14:45:51 +0900

    지도 api 회원가입 부분 완성

--- src/main/java/com/green/momoolggo/application/address/model/UserAddressReq.java
+++ src/main/java/com/green/momoolggo/application/address/model/UserAddressReq.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserAddressReq {
+    private Long UserNo;
     private String address;        // 기본주소
     private String addressDetail;  // 상세주소
     private Double lat;            // 위도
