351380344c5a32db09c1033b05f9d5991a8ae35c
inha915 <dlsgk0915@gmail.com>
2026-03-20 17:46:48 +0900

    로직 일부 수정

--- src/main/java/com/green/momoolggo/application/order/model/OrderHistoryDto.java
+++ src/main/java/com/green/momoolggo/application/order/model/OrderHistoryDto.java
@@ -12,6 +12,7 @@ public class OrderHistoryDto {
     private String date;           // 포맷된 날짜 "3월 15일(일)"
     private String storeName;
     private String storeImage;
+    private long storeId;
     private int totalPrice;        // orders.amount
     private int deliveryFee;       // orders.delivery_fee
     private long orderId;          // orders.order_id
